f8a7e2f57d0811a6895e4a8377e5e37a6b00fca1
vixandrade <vix.andrade21@gmail.com>
2022-01-19 12:56:25 -0300

    feat: using GameState data class in StateT Monad

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # hangman-vix
 Haskell implementation of Hangman – my way!
+
+The Hangman game is a classic exercise to study Functional Programming, as its game loop can be easily implemented with a recursive function, with clear conditions for ending the game.
+
+I've changed the rules of the game to make it complex enough to introduce Monads and Transformers in the exercise.
+
+This version has two players competing for the highest score – each correct guess adds to the score depending on the guessed letter.
+
+The word to be used as solution is loaded from a dictionary file. A of the guesses and awarded points is shown at the end of the game.
--- app/Main.hs
+++ app/Main.hs
@@ -7,8 +7,8 @@ import Control.Monad.Trans.Writer.CPS
 import Data.Char (toUpper)
 import Data.List
 import Data.Text (Text, pack)
-import System.IO
 import System.Console.ANSI
+import System.IO
 
 ----
 
@@ -17,26 +17,61 @@ import System.Console.ANSI
 -- Run GameTurns recursively until guesses match Solution (State)
 -- In each turn, if user guesses correct letter, accumulates Score (Writer)
 
-type Turn = WriterT Text (ReaderT String (StateT [Char] IO))
+data GameState = GameState
+  { guessed :: [Char],
+    currentPlayer :: Bool,
+    scores :: (Int, Int)
+  } deriving (Show)
+
+newGame :: GameState
+newGame = GameState {
+  guessed = [],
+  currentPlayer = True,
+  scores = (0,0)
+}
+
+type Turn = WriterT Text (ReaderT String (StateT GameState IO))
 
 runTurn :: Turn String
 runTurn = do
   solution <- lift ask
-  guesses <- lift get
+  currentState <- lift get
+  let guesses = guessed currentState
+  let player = currentPlayer currentState
   let masked = getMasked solution guesses
   if masked == solution
     then return "Success"
     else do
+      lift $ lift $ lift $ print currentState
       lift $ lift $ lift $ print "=============================="
       lift $ lift $ lift $ print masked
-      lift $ lift $ lift $ print "Guess a letter:"
+      lift $ lift $ lift $ print (getPlayerName player ++ ": Guess a letter:")
       guess <- lift $ lift $ lift getChar
       let score = getScore guess guesses solution
       tell $ pack (toUpper guess : ": was guessed for " ++ show score ++ " points! \n")
       let newGuesses = guess : guesses
-      lift $ put newGuesses
+      lift $ modify (updateState score newGuesses)
       lift $ lift $ lift clearScreen
       runTurn
+
+updateState :: Int -> [Char] -> GameState -> GameState
+updateState score guesses state =
+  GameState
+    { guessed = guesses,
+      currentPlayer = switchPlayer $ currentPlayer state,
+      scores = addScore score (currentPlayer state) (scores state)
+    }
+  where
+    switchPlayer :: Bool -> Bool
+    switchPlayer player = not player
+    addScore :: Int -> Bool -> (Int, Int) -> (Int, Int)
+    addScore score player (p1Score, p2Score)
+      | player = (p1Score + score, p2Score)
+      | otherwise = (p1Score, p2Score + score)
+
+getPlayerName :: Bool -> String
+getPlayerName True = "Player 1"
+getPlayerName False = "Player 2"
 
 getMasked :: String -> [Char] -> String
 getMasked solution guesses = [if c `elem` guesses then c else '_' | c <- solution]
@@ -51,8 +86,8 @@ getScore c guesses solution
 runGame :: IO ()
 runGame = do
   hSetBuffering stdin NoBuffering
-  ((result, log), state) <- runStateT (runReaderT (runWriterT runTurn) "botequim") []
-  print log
+  ((result, log), state) <- runStateT (runReaderT (runWriterT runTurn) "botequim") newGame
+  print state
 
 main :: IO ()
 main = runGame
