7ed37736430c6ae23e8eb99a553366a2fde0dde7
vixandrade <vix.andrade21@gmail.com>
2022-01-19 15:55:17 -0300

    fix: set NoBuffering to 'Go Again' menu

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,14 @@
 module Main where
 import System.Console.ANSI
 import Control.Monad.Trans.Maybe
+import System.IO
 
 import Game.Auxiliary (getSolution)
 import Game.Runner (runGame)
 
 main :: IO ()
 main = do
+  hSetBuffering stdin NoBuffering
   clearScreen
   putStrLn "CHOOSE A WORDLIST: (batch37, movies, premierleague):"
   wordlist <- getLine 
